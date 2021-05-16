07e5aed00137226e4c41bcc779646de180ca1078
Mollie Dickenson <molliedickenson@gmail.com>
2021-05-15 23:55:27 -0400

    commit tree 3adbb4c29ac560d49bf05c9d7ea9624a8ad3b257

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,16 @@
 def new_hash
-  # return an empty hash
+ a_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  a_hash = {color: "red"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ new_hash = {name: "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  my_hash = {id: number}
 end
